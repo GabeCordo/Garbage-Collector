f077b551e7324429fad7a25b2baabf77bdb579ea
Gabriel Cordovado <gabe.cordovado@yahoo.ca>
2020-06-28 21:31:17 -0400

    added the structure database which stores all structs implemented by the application at compilation time

---
+++ gc/collector.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "collector.h"
+
+/*
+ * Function:  struct_record_print
+ * ------------------------------
+ * prints the data within the struct record in a
+ * visually apealing format for debugging
+ *
+ *  *record: a pointer to the record type
+ */
+void struct_record_print(struct_record_t *record) {
+	
+	printf("Record (%p)", &record);
+	printf("%s\t| size = %i\t| fields = %i", record->name, record->size, record->num_of_fields);
+	
+}
+
+/*
+ * Function:  struct_database_print
+ * --------------------------------
+ * prints the data within the struct database in a
+ * visually apealing format for debugging with as-
+ * istance of the struct_record_print function
+ *
+ *  *database: the database we will iterate over for
+ *			   each record we will print
+ */
+void struct_database_print(struct_database_t *database) {
+	
+	printf("# of Structures Registered = %i", database->num_of_records);
+	
+	struct_record_t *struct_ptr = NULL;
+	ITERATE_STRUCT_RECORDS_BEGIN(database, struct_ptr) {
+		
+		struct_record_print(struct_ptr);
+		
+	} ITERATE_STRUCT_RECORDS_END;
+		
+}
+
+/*
+ * Function:  struct_record_add
+ * ----------------------------
+ * adds a new record into the structure database, ensuring
+ * that the appropriate pointers will be changed where re-
+ * quired
+ *
+ *  *database: the struct database we will add the new record to
+ *	*new: the new struct record we will add to the list of records
+ *
+ *  returns: 0, which will avoid the macro asserting a failure
+ */
+int struct_record_add(struct_database_t *database, struct_record_t *new) {
+	
+	struct_record_t *head = database->head;
+	//check to see if the struct database has a head
+	if(!head) {
+		database->head = new;
+		new->next = NULL;
+		database->num_of_records++;
+	}
+	
+	//else replace the new record as the head
+	new->next = head;
+	database->head = new;
+	database->num_of_records++;
+	return 0;
+}
+
+/*
+ * Function:  struct_database_lookup
+ * -----------------------------------------
+ * looks for a given record within the structures database
+ * by matching the array of chars argument with the one
+ * stored within the record type
+ *
+ *  *database: the struct database we will be looking in
+ *	*name: the name of the struct record to lookup
+ *
+ *  returns: the pointer to the struct record that matches
+ *			 the name argument passed to the function
+ */
+struct_record_t* struct_database_lookup(struct_database_t *database, char *name) {
+	
+	struct_record_t *struct_ptr = NULL;
+	ITERATE_STRUCT_RECORDS_BEGIN(database, struct_ptr) {
+		
+		if(!strcmp(name, struct_ptr->name))
+			return struct_ptr;
+		
+	} ITERATE_STRUCT_RECORDS_END;
+	
+	return NULL;
+}
---
+++ gc/collector.h
@@ -0,0 +1,85 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+	
+	/* constants */
+	
+	#define MAXIMUM_NAME_DESCRIPTOR 120
+	
+	/* type deffinitions */
+	
+	typedef enum types {
+		UINT8,
+		UINT32,
+		INT32,
+		CHAR,
+		OBJ_PTR,
+		FLOAT,
+		DOUBLE,
+		OBJ_STRUCT
+	} types_t;
+	
+	typedef struct struct_field_ {
+		char name[MAXIMUM_NAME_DESCRIPTOR];
+		types_t data_type; //one of the enum types described by types_t
+		unsigned int size;
+		unsigned int offset;
+		char name_structure[MAXIMUM_NAME_DESCRIPTOR]; //useful for OBJ_PTR and OBJ_STRUCT
+	} struct_field_t;
+	
+	typedef struct _struct_record_t struct_record_t;
+	
+	struct _struct_record_t {
+		struct_record_t *next; //pointer to the next struct record in the linked list
+		char name[MAXIMUM_NAME_DESCRIPTOR];
+		unsigned int size; //size of the structure
+		unsigned int num_of_fields; //number of fields the structure contains
+		struct_field_t *fields; //pointer to an array of fields
+	};
+	
+	typedef struct struct_database_ {
+		struct_record_t *head; //linked list of struct records
+		unsigned int num_of_records;
+	} struct_database_t;
+	
+	/* macro deffinitions */
+	
+	#define OFFSET_OF(struct_name, field_name)	\
+		(unsigned int)&(((struct_name *)0)->field_name) //zero pointer to the begining to the struct till the field-name
+		
+	#define SIZE_OF_FIELD(struct_name, field_name)	\
+		sizeof(((struct_name *)0)->field_name) //compute the size of the field within the struct argument
+		
+	#define FIELD_INFO(struct_name, field_name, type_name, nested_struct_name)	\
+		{#field_name, 															\
+		 type_name,																\
+		 SIZE_OF_FIELD(struct_name, field_name),								\
+		 OFFSET_OF(struct_name, field_name),									\
+		 #nested_struct_name}													
+	
+	#define RECORD_INSERT(database_name, struct_name, fields_array)					\
+		do {																		\
+			struct_record_t *record = calloc(1, sizeof(struct_record_t));			\
+			strncpy(record->struct_name, #struct_name, MAXIMUM_NAME_DESCRIPTOR);	\
+			record->size = sizeof(struct_name);										\
+			record->fields = sizeof(fields_array) / sizeof(struct_field_t);			\
+			record->fields = fields_array;											\
+			if(struct_record_add(database_name, record)) {							\
+				assert(0);															\
+			}																		\
+		} while(0);
+		
+	#define ITERATE_STRUCT_RECORDS_BEGIN(struct_database, struct_ptr)					\
+	{																					\
+		for(struct_ptr = struct_database->head;											\
+			struct_ptr;																	\
+			struct_ptr = struct_ptr->next) {											\
+	
+	#define ITERATE_STRUCT_RECORDS_END }}
+	
+	/* function deffinitions */
+	
+	void struct_record_print(struct_record_t *record); //needs to further expand
+	void struct_database_print(struct_database_t *database);
+	int struct_record_add(struct_database_t *database, struct_record_t *new);
+
+#endif /* HEADER_FILE */
